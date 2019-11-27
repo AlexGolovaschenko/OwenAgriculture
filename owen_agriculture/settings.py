841d1805979cf624797c72335988a5974330460f
Aleksandr <golovasanya@mail.ru>
2019-11-27 13:35:03 +0200

    fixed code formatting

--- owen_agriculture/settings.py
+++ owen_agriculture/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 PROJECT_ROOT = os.path.join(BASE_DIR, 'owen_agriculture')
 
 # Quick-start development settings - unsuitable for production

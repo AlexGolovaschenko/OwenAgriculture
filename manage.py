215cb1f360f1c55f26d6a8a334f48dee3d83d190
Aleksandr <golovasanya@mail.ru>
2019-11-27 11:48:06 +0200

    renamed packages to lower case

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OwenAgriculture.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'owen_agriculture.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

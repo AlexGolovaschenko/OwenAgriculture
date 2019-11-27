7886fddbdcf6abaea2fbe0cb1e46fc45d12b3635
Aleksandr <golovasanya@mail.ru>
2019-11-27 14:52:57 +0200

    fixed migrations

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200)),
                 ('description', models.TextField()),
-                ('topic', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='Products.ProductTopic')),
+                ('topic', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.ProductTopic')),
             ],
         ),
     ]

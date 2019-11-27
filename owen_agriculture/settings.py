adf5388389f54266df1c0dc270e4a095b938f0f2
Aleksandr <golovasanya@mail.ru>
2019-11-27 15:34:14 +0200

    in base.html included w3.css style and title icon

--- owen_agriculture/settings.py
+++ owen_agriculture/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT)
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, "static")
+]

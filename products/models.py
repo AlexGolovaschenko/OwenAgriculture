5eba24f38e2411af99afd5230b484ddc7f2af16a
Aleksandr <golovasanya@mail.ru>
2019-11-29 19:03:48 +0200

    made catalog template and poultry_catalog view

--- products/models.py
+++ products/models.py
@@ -2,17 +2,26 @@ from django.db import models
 
 
 class ProductTopic (models.Model):
-	headline = models.CharField(max_length=200)
-	description = models.TextField()
+	headline = models.CharField(verbose_name='Название', max_length=200)
+	description = models.TextField(verbose_name='Описание')
+	order = models.PositiveSmallIntegerField(verbose_name='Порядок отображения', default=0)
 
 	def __str__ (self):
 		return self.headline
 
+	class Meta():
+		verbose_name = "Категория продукта"
+		verbose_name_plural = "Категории продуктов"
+
 
 class Product (models.Model):
-	topic = models.ForeignKey(ProductTopic, on_delete=models.CASCADE)
-	name = models.CharField(max_length=200)
-	description = models.TextField()
+	topic = models.ForeignKey(ProductTopic, verbose_name='Категория продукта', on_delete=models.CASCADE)
+	name = models.CharField(verbose_name='Наименование', max_length=200)
+	description = models.TextField(verbose_name='Описание')
 
 	def __str__ (self):
 		return self.name
+
+	class Meta():
+		verbose_name = "Продукт"
+		verbose_name_plural = "Продукты"
--- products/views.py
+++ products/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from products.models import ProductTopic, Product
 
 def poultry_catalog(request):
-	return render(request, 'products/poultry_catalog.html')
+	topics = ProductTopic.objects.order_by('order')
+	items = {}
+	for topic in topics:
+		items[topic.headline] = topic.product_set.order_by('name')[:16] 
+
+	context = {'topics': topics, 'items': items}
+
+	return render(request, 'products/catalog.html', context)
 
 def pigsty_catalog(request):
-	return render(request, 'products/pigsty_catalog.html')
+	return render(request, 'products/catalog.html')

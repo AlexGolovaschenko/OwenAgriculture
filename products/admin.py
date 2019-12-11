1f63b6d7e71b7ebae6e7d67762b211c9414e2c0c
Aleksandr <golovasanya@mail.ru>
2019-12-11 16:08:31 +0200

    reworked design of news list template, added product specification model, customized product display on admin page, upload w3.css file to static

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from .models import ProductTopic, Product, TopicsGroup
+from .models import ProductTopic, Product, TopicsGroup, ProductSpecification
 
-admin.site.register(ProductTopic)
-admin.site.register(Product)
-admin.site.register(TopicsGroup)
+
+class ProductSpecificationInline (admin.StackedInline):
+	model = ProductSpecification
+
+class ProductAdmin (admin.ModelAdmin):
+	list_display = ['name', 'model', 'topic', 'used_in_poultry', 'used_in_pigsty']
+	inlines = [ProductSpecificationInline]
+
+class TopicsGroupAdmin (admin.ModelAdmin):
+	list_display = ['headline', 'order']
+
+class ProductTopicAdmin (admin.ModelAdmin):
+	list_display = ['headline', 'order']
+
+admin.site.register(ProductTopic, ProductTopicAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(TopicsGroup, TopicsGroupAdmin)
--- products/models.py
+++ products/models.py
@@ -15,12 +15,26 @@ class ProductTopic (models.Model):
 		verbose_name_plural = "Категории продуктов"
 
 
+class TopicsGroup (models.Model):
+	headline = models.CharField(verbose_name='Название', max_length=200)
+	order = models.PositiveSmallIntegerField(verbose_name='Порядок отображения', default=0)
+	topics = models.ManyToManyField(ProductTopic, verbose_name='Категории продуктов')
+
+	def __str__ (self):
+		return self.headline
+
+	class Meta():
+		verbose_name = "Группа"
+		verbose_name_plural = "Группы"
+
+
 class Product (models.Model):
 	topic = models.ForeignKey(ProductTopic, verbose_name='Категория продукта', on_delete=models.CASCADE)
-	name = models.CharField(verbose_name='Наименование', max_length=100, default='')
+	name = models.CharField(verbose_name='Полное наименование', max_length=100, blank=False)
+	model = models.CharField(verbose_name='Модель', max_length=50, blank=False)
 	image = models.ImageField(default='product_default.png', upload_to='product_pictures')
 	short_description = models.CharField(verbose_name='Краткое описание', max_length=200, default='')
-	description = models.TextField(verbose_name='Полное описание', default='')
+	description = models.TextField(verbose_name='Описание', default='')
 	used_in_poultry = models.BooleanField(verbose_name='Используется в птицекомплексах', default=False)
 	used_in_pigsty = models.BooleanField(verbose_name='Используется в свинокомплексах', default=False)
 
@@ -32,14 +46,23 @@ class Product (models.Model):
 		verbose_name_plural = "Продукты"
 
 
-class TopicsGroup (models.Model):
-	hedline = models.CharField(verbose_name='Название', max_length=200)
-	order = models.PositiveSmallIntegerField(verbose_name='Порядок отображения', default=0)
-	topics = models.ManyToManyField(ProductTopic, verbose_name='Категории продуктов')
+class ProductSpecification (models.Model):
+	product = models.OneToOneField(
+	        Product,
+	        on_delete=models.CASCADE,
+	        primary_key=True,
+	    )
+	specifications = models.TextField(verbose_name='Технические характеристики', blank=True) # should be table
+	func_scheme = models.ImageField(verbose_name='Функциональная схема', upload_to='product_schemes', blank=True)
+	сontrols_description = models.TextField(verbose_name='Элементы управления', blank=True)
+	dimensions = models.TextField(verbose_name='Габаритные размеры', blank=True) # should be table
+	connection_scheme = models.ImageField(verbose_name='Cхемы подключения', upload_to='product_schemes', blank=True) # can be many images (OneToMany ????)
+	documentation = models.TextField(verbose_name='Документация', blank=True) # should be html template with lincs for download file
+	package_bundle = models.TextField(verbose_name='Комплектация', blank=True)
 
 	def __str__ (self):
-		return self.hedline
+		return 'Specification ' + self.product.name
 
 	class Meta():
-		verbose_name = "Группа"
-		verbose_name_plural = "Группы"
+		verbose_name = "Спецификация продукта"
+		verbose_name_plural = "Спецификации продуктов"

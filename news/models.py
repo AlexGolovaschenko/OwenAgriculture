35c83a5ba49967e1a44e067280fbacd3500288d2
Golovaschenko Alex <sa.golovaschenko@gmail.com>
2020-05-28 12:59:47 +0300

    added pub_date t onews model

--- news/models.py
+++ news/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+import datetime
+from PIL import Image
 
 class Article(models.Model):
 	header = models.CharField(max_length=200, verbose_name='Название')
 	image = models.ImageField(upload_to='news', blank=True, verbose_name='Рисунок')
-	short_description = models.CharField(max_length=200, verbose_name='Краткое описание')	 
-	text = models.TextField(verbose_name='Текст')
+	short_description = models.TextField(verbose_name='Краткое описание')	 
+	text = RichTextUploadingField(verbose_name='Текст')
+	pub_date = models.DateField(verbose_name='Дата публикации', default=datetime.date.today)
 
 	def __str__ (self):
 		return self.header
@@ -12,3 +16,12 @@ class Article(models.Model):
 	class Meta():
 		verbose_name = "Новость"
 		verbose_name_plural = "Новости"
+
+	def save(self):
+		super().save()
+		if self.image:
+			img = Image.open(self.image.path)
+			if img.height > 600 or img.width > 600:
+				output_size = (600, 600)
+				img.thumbnail(output_size)
+				img.save(self.image.path)

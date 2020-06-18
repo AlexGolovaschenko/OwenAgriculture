8daab1f9cc4cfd62a32dc414bb396c061ae2facf
Aleksandr <golovasanya@mail.ru>
2020-06-18 12:46:06 +0300

    displayed image url on banners admin page

--- banners/admin.py
+++ banners/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Banner
 
 class BannerAdmin (admin.ModelAdmin):
-	list_display = ['name', 'order', 'display']
+	list_display = ['name', 'banner_image', 'display', 'order']
 
 admin.site.register(Banner, BannerAdmin)

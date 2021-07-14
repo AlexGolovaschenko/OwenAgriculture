673b720f679416c80dbf31d865c4addf07f0ca75
Aleksandr <golovasanya@mail.ru>
2021-07-14 16:21:18 +0300

    improve news and projects admin

--- news/admin.py
+++ news/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Article
 
-admin.site.register(Article)
+class NewsAdmin (admin.ModelAdmin):
+	list_display = ['header', 'display', 'pub_date']
+
+admin.site.register(Article, NewsAdmin)
+
--- projects/admin.py
+++ projects/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import ProjectArticle
 
-admin.site.register(ProjectArticle)
+
+
+class ProjectAdmin (admin.ModelAdmin):
+	list_display = ['header', 'display']
+
+admin.site.register(ProjectArticle, ProjectAdmin)
+

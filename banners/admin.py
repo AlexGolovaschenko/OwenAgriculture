ab8bac784b7116aa5bb02cef3e103798e17d35ce
Aleksandr <golovasanya@mail.ru>
2021-07-14 16:32:42 +0300

    improve banners admin

--- banners/admin.py
+++ banners/admin.py
@@ -4,5 +4,6 @@ from .models import Banner
 class BannerAdmin (admin.ModelAdmin):
 	list_display = ['name', 'banner_image', 'display', 'order']
 	list_editable = ['display', 'order']
+	ordering = ['order']
 
 admin.site.register(Banner, BannerAdmin)
